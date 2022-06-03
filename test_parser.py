9c27b94aff0d31a23421342463b252f42b20fde9
jsw66 <67905915+jsw66@users.noreply.github.com>
2022-06-03 01:25:01 +0100

    work on parser tests

--- test_parser.py
+++ test_parser.py
@@ -1,23 +1,31 @@
 import pytest
 import os
+from pathlib import Path
+
 from names import Names
-from parse2 import Parser
+from parse import Parser
 from scanner import Scanner
 from devices import Devices
 from network import Network
 from monitors import Monitors
 
-def new_parser(path):
+
+def dummy_parser(path):
     """Return a new parser instance."""
     new_names = Names()
     new_scanner = Scanner(path, new_names)
     new_devices = Devices(new_names)
     new_network = Network(new_names, new_devices)
     new_monitors = Monitors(new_names, new_devices, new_network)
-    new_parser = Parser(new_names, new_devices, new_network, new_monitors, new_scanner)
+    new_parser = Parser(new_names, new_devices, new_network,
+                        new_monitors, new_scanner)
     return new_parser
 
-def test_parser1():
-    path1 = os.getcwd() + "\\test_scanner1.txt"
-    test_parser1 = new_parser(path1)
-    assert test_parser1.parse_network() == True
+
+@pytest.mark.parametrize("path", [
+    "test_files/parser_test_1.txt"
+])
+def test_parser_returns_true(path):
+    """Tests if the parser returns True for correct definition files"""
+    parser = dummy_parser(str(Path(path)))
+    assert parser.parse_network() is True
