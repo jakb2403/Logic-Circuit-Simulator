cb3e5e43031d189499b4fade6f5a8a2d332b35fa
hyunwinn <hscho0810@gmail.com>
2022-06-07 13:48:54 +0100

    changes to parse.py for debugging in different laptop

--- parse_debug.py
+++ parse_debug.py
@@ -6,13 +6,14 @@ from devices import Devices
 from network import Network
 from monitors import Monitors
 
-path=str(Path("test_files/perfect_file.txt"))
-print(path)
+path = str(Path("test_parse1.txt"))
+
 new_names = Names()
 new_scanner = Scanner(path, new_names)
 new_devices = Devices(new_names)
 new_network = Network(new_names, new_devices)
 new_monitors = Monitors(new_names, new_devices, new_network)
-new_parser = Parser(new_names, new_devices, new_network, new_monitors, new_scanner)
-
-new_parser.parse_network()
+new_parser = Parser(new_names, new_devices, new_network, new_monitors,
+                    new_scanner)
+new_parser.symbol = new_parser.scanner.get_symbol()
+new_parser._section_devices()
